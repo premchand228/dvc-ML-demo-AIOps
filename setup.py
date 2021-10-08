755620ebbafe86873979751056cf3df5ade24b23
premchand228 <pream.chand228@gmail.com>
2021-10-08 13:34:27 +0530

    pushing setup,py changes to remote

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="src",
     version="0.0.1",
-    author="c17hawke",
+    author="premchand",
     description="A small package for dvc ml pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/c17hawke/dvc-ML-demo-AIOps",
-    author_email="sunny.c17hawke@gmail.com",
+    url="https://github.com/premchand228/dvc-ML-demo-AIOps",
+    author_email="pream.chand228@gmail.com",
     packages=["src"],
     python_requires=">=3.7",
     install_requires=[
