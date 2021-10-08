459c902cb984f6ea57501a2fed43c633154b4dad
premchand228 <pream.chand228@gmail.com>
2021-10-08 13:25:18 +0530

    commiting push changes

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -8,7 +8,7 @@ def get_data(config_path):
 
     remote_data_path = config["data_source"]
     df = pd.read_csv(remote_data_path, sep=";")
-    print(df.head())
+    #print(df.head())
 
     # Save local dataset in the path
     # create path to local directory : artifacts/raw_local_dir/data.csv
